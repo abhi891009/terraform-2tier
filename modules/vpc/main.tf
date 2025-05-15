83895e144489ba489871a111918a145e30e9e8aa
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-15 09:28:02 +0000

    Running the two tier architectuer CI/CD pipeline

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,27 +5,27 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
 }
 
 resource "aws_security_group" "web_sg" {
